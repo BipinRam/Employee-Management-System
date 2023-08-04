3dd40892412cf4565b524e77a5ca1801a73415ad
BipinRam <bipinram25@gmail.com>
2023-08-04 13:31:57 +0530

    EmployeeSalary

--- src/main/java/com/example/demo/repository/SalaryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Salary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalaryRepository extends JpaRepository<Salary , Long> {
-    Salary findByEmployeeIdAndMonthAndYear (Long employeeId , String Month , int year);
-}
