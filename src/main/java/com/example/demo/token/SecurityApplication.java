c279c39658cc34ee1fd6800b36c90f98cc16cd90
BipinRam <139105885+BipinRam@users.noreply.github.com>
2023-08-22 16:54:57 +0530

    Update SecurityApplication.java

--- src/main/java/com/example/demo/token/SecurityApplication.java
+++ src/main/java/com/example/demo/token/SecurityApplication.java
@@ -1,11 +1,1 @@
-package com.example.demo.token;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SecurityApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SecurityApplication.class, args);
-    }
-}
