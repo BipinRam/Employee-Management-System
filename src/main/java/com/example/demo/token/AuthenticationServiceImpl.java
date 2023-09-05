3f7337c3b899b3094cb383726e4b7828cd593217
BipinRam <bipinram25@gmail.com>
2023-09-05 14:47:40 +0530

    token generation complete

--- src/main/java/com/example/demo/token/AuthenticationServiceImpl.java
+++ src/main/java/com/example/demo/token/AuthenticationServiceImpl.java
@@ -11,10 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @RequiredArgsConstructor
@@ -40,9 +37,10 @@ public class AuthenticationServiceImpl implements AuthenticationService{
 
     @Override
     public JwtAuthenticationResponse signIn(SignInRequest request) {
+        Optional<User> users = userRepository.findByEmail(request.getEmail());
         try{
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+                    new UsernamePasswordAuthenticationToken(users.get().getUsername(), request.getPassword()));
         }catch (AuthenticationException exception){
             exception.printStackTrace();
             throw new IllegalArgumentException("Invalid email or password.");
--- src/main/java/com/example/demo/token/SignInRequest.java
+++ src/main/java/com/example/demo/token/SignInRequest.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignInRequest {
+
     private String email;
     private String password;
 }
