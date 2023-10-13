1aa8efa6cee75669b7a82c7ba0d0ae88591796cf
BipinRam <bipinram25@gmail.com>
2023-10-13 14:48:54 +0530

    Asset Assign Service

--- src/main/java/com/example/demo/service/AssetAssignService.java
+++ src/main/java/com/example/demo/service/AssetAssignService.java
@@ -10,10 +10,14 @@ import com.example.demo.repository.AssetAssignRepository;
 import com.example.demo.repository.AssetRepository;
 import com.example.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AssetAssignService {
@@ -34,50 +38,57 @@ public class AssetAssignService {
         assetAssign.setAssignDate(assetAssignModel.getAssignDate());
         assetAssign.setAssignRemarks(assetAssignModel.getAssignRemarks());
 
-            Optional<Employee> employeeOptional = employeeRepository.findById(assetAssignModel.getEmployeeId());
-            Optional<Asset> assetOptional = assetRepository.findById(assetAssignModel.getAssetId());
-            if (employeeOptional.isPresent()) {
-                if (assetOptional.isPresent()) {
-                    assetAssign.setEmployee(employeeOptional.get());
-                    assetAssign.setAsset(assetOptional.get());
+        Optional<Employee> employeeOptional = employeeRepository.findById(assetAssignModel.getEmployeeId());
+        Optional<Asset> assetOptional = assetRepository.findById(assetAssignModel.getAssetId());
+        if (employeeOptional.isPresent()) {
+            if (assetOptional.isPresent()) {
+                assetAssign.setEmployee(employeeOptional.get());
+                assetAssign.setAsset(assetOptional.get());
 
-                    EmployeeModel employeeModel = new EmployeeModel();
-                    employeeModel.setId(assetAssign.getEmployee().getId());
-                    employeeModel.setEmployeeFullName(assetAssign.getEmployee().getEmployeeFullName());
-                    employeeModel.setEmail(assetAssign.getEmployee().getEmail());
-                    employeeModel.setBloodGroup(assetAssign.getEmployee().getBloodGroup());
-                    assetAssignModel.setEmployeeModel(employeeModel);
+                EmployeeModel employeeModel = new EmployeeModel();
+                employeeModel.setId(assetAssign.getEmployee().getId());
+                employeeModel.setEmployeeFullName(assetAssign.getEmployee().getEmployeeFullName());
+                employeeModel.setEmail(assetAssign.getEmployee().getEmail());
+                employeeModel.setBloodGroup(assetAssign.getEmployee().getBloodGroup());
+                assetAssignModel.setEmployeeModel(employeeModel);
 
-                    AssetModel assetModel = new AssetModel();
-                    assetModel.setId(assetAssign.getAsset().getId());
-                    assetModel.setSerialNo(assetAssign.getAsset().getSerialNo());
-                    assetModel.setModelName(assetAssign.getAsset().getModelName());
-                    assetModel.setManufacturingDate(assetAssign.getAsset().getManufacturingDate());
-                    assetModel.setSpecification(assetAssign.getAsset().getSpecification());
-                    assetModel.setAddedDate(assetAssign.getAsset().getAddedDate());
-                    assetAssignModel.setAssetModel(assetModel);
-                } else {
-                    throw new Exception("Invalid asset id");
-                }
+                AssetModel assetModel = new AssetModel();
+                assetModel.setId(assetAssign.getAsset().getId());
+                assetModel.setSerialNo(assetAssign.getAsset().getSerialNo());
+                assetModel.setModelName(assetAssign.getAsset().getModelName());
+                assetModel.setManufacturingDate(assetAssign.getAsset().getManufacturingDate());
+                assetModel.setSpecification(assetAssign.getAsset().getSpecification());
+                assetModel.setAddedDate(assetAssign.getAsset().getAddedDate());
+                assetAssignModel.setAssetModel(assetModel);
             } else {
-                throw new Exception("invalid employee id");
+                throw new Exception("Invalid asset id");
             }
+        } else {
+            throw new Exception("invalid employee id");
+        }
 
 
         if (assetAssigns.isEmpty()) {
             assetAssignRepository.save(assetAssign);
         } else {
+
+            boolean flag = false;
             for (AssetAssign a : assetAssigns) {
-                if (a.getReturnDate() != null) {
-                    assetAssignRepository.save(assetAssign);
-                } else {
-                    throw new Exception("Asset is already assigned to another employee");
+
+                if (a.getReturnDate() == null) {
+                    flag = true;
+
                 }
             }
-
+            if (!flag) {
+                assetAssignRepository.save(assetAssign);
+            } else {
+                throw new Exception("Asset is already assigned to another employee");
+            }
         }
-        assetAssignModel.setId(assetAssign.getId());
-        return assetAssignModel;
+            assetAssignModel.setId(assetAssign.getId());
+            return assetAssignModel;
+
     }
 //    UPDATE
 
@@ -104,5 +115,5 @@ public class AssetAssignService {
             throw new Exception("Invalid_id");
         }
     }
-
+    
 }
